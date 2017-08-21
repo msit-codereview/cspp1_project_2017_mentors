51e0e37dae9ef855d7bb1b7d28b2befd0973ac11
rehana-msit <rehana.msit@gmail.com>
2017-08-21 17:10:46 +0530

    second commit

--- plag.py
+++ plag.py
@@ -1,6 +1,7 @@
 import math
 f1=raw_input("file1:")
 f2=raw_input("file2:")
+f3=raw_input("file3:")
 Matrix={}
 
 def freqcount(f):
@@ -17,28 +18,47 @@ def freqcount(f):
 
 mydict={}
 mydict2={}
+mydict3={}
 mydict=freqcount(f1)
 mydict2=freqcount(f2)
-f1count=0
-f2count=0
-sum=0
+mydict3=freqcount(f3)
+
+
 print mydict
 print mydict2
-for i in mydict.values():
-    f1count+=i*i
-for j in mydict2.values():
-     f2count+=j*j    
-for i in mydict.keys():
-    for j in mydict2.keys():            
-        if i==j:
-            #print("word:"+(i))
-            a=mydict[i]
-            b=mydict2[j]
-            sum=sum+a*b
-        
-print sum
+print mydict3
+
+def euclid(d):
+    count=0
+    for i in d.values():
+        count=count+i*i
+    return math.sqrt(count)
+
+f1count=0
+f2count=0
+f3count=0
+f1count=euclid(mydict)
 print f1count
+f2count=euclid(mydict2)
 print f2count
-entropy=sum/(math.sqrt(f1count)*math.sqrt(f2count))
+f3count=euclid(mydict3)
+print f3count
+
+def dp(d1,d2):
+    sum=0
+    for i in d1.keys():
+        for j in d2.keys():            
+            if i==j:                
+                a=d1[i]
+                b=d2[j]
+                sum=sum+a*b
+        
+    return sum
+
+entropy=sum/(f1count*f2count)
 print entropy
-Matrix=[[0,entropy],[0,0]]
+Matrix=[[0,dp(mydict,mydict2)/(euclid(mydict)*euclid(mydict2)),dp(mydict,mydict3)/(euclid(mydict)*euclid(mydict3))],
+        [dp(mydict,mydict2)/(euclid(mydict)*euclid(mydict2)),0,dp(mydict2,mydict3)/(euclid(mydict2)*euclid(mydict3))],
+         [dp(mydict,mydict3)/(euclid(mydict)*euclid(mydict3)),dp(mydict2,mydict3)/(euclid(mydict2)*euclid(mydict3)),0]]
+print Matrix
+print 
